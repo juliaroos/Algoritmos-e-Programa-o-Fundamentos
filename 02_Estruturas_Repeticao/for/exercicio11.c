daf3324a1d57f32f4564d6a0d430e1dd3a8ba43e
Júlia Roos costa <ju.rooscosta@gmail.com>
2026-07-06 15:15:27 -0300

    Create exercicio11.c

---
+++ 02_Estruturas_Repeticao/for/exercicio11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+       int cont; 
+    for (cont = 0; cont < 20; cont++){
+        double valor;
+        printf("Digite um valor com ponto flutuante: ");
+        scanf ("%lf", &valor);
+    }
+
+    return 0;
+}
