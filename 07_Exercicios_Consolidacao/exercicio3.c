a6b961e27f2cd24c0f7bff56b2c6fe981fd5281c
Júlia Roos costa <ju.rooscosta@gmail.com>
2026-07-15 16:13:41 -0300

    Create exercicio3.c

---
+++ 07_Exercicios_Consolidacao/exercicio3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void mostrapalavra(char *palavra) {
+    printf("%s\n", palavra);
+}
+
+
+int main() {
+    
+ char letras[] = "QUESTAO 3";
+ mostrapalavra(letras);
+
+    return 0;
+}
